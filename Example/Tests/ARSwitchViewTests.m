48d54efc3b1237aa38fd1befbff4bbfab812fd8c
Maxim Cramer <mennenia@gmail.com>
2016-06-02 12:10:21 +0100

    [SwitchView] Add tests for order-independent SwitchViewSetup

--- Example/Tests/ARSwitchViewTests.m
+++ Example/Tests/ARSwitchViewTests.m
@@ -34,4 +34,28 @@ it(@"adjusts buttons to any switch width", ^{
   expect(switchView).to.haveValidSnapshot();
 });
 
+
+it(@"has the right index selected", ^{
+    NSArray *titles = @[@"First title", @"Second Title", @"Third Title"];
+    
+    ARSwitchView *switchView = [ARSwitchView new];
+    switchView.frame = (CGRect){.origin = CGPointZero, .size = CGSizeMake(728, switchView.intrinsicContentSize.height)};
+    switchView.titles = titles;
+    switchView.selectedIndex = 1;
+    [switchView snapshotViewAfterScreenUpdates:YES];
+    expect(switchView).to.haveValidSnapshot();
+});
+
+it(@"has the right index selected when setting titles later on", ^{
+    NSArray *titles = @[@"First title", @"Second Title", @"Third Title"];
+    
+    ARSwitchView *switchView = [ARSwitchView new];
+    switchView.frame = (CGRect){.origin = CGPointZero, .size = CGSizeMake(728, switchView.intrinsicContentSize.height)};
+
+    switchView.selectedIndex = 1;
+    switchView.titles = titles;
+    [switchView snapshotViewAfterScreenUpdates:YES];
+    expect(switchView).to.haveValidSnapshot();
+});
+
 SpecEnd;
